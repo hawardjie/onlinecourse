17301436b2784bfe64ffd5e34219136c5157fb7d
zhihao <zhihaoapp@gmail.com>
2022-06-08 05:42:32 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className="player-wrapper">
+      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/9Tx631VnH40' />
+    </div>
+    <div className="player-wrapper">
+      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/ajj35CJ4S5E' />
+    </div>
+    <div className="player-wrapper">
+      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/rTsa8v35PA8' />
+    </div>
     </div>
   );
 }
