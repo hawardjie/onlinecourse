81de9e368f81a39aab4d755d08c9914dc8543c2d
zhihao <zhihaoapp@gmail.com>
2022-06-08 23:53:37 -0700

    Add 2022-06-05 Add batch 1 clips

--- src/App.js
+++ src/App.js
@@ -4,15 +4,33 @@ import './App.css';
 function App() {
   return (
     <div>
-    <div className="player-wrapper">
-      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/9Tx631VnH40' />
-    </div>
-    <div className="player-wrapper">
-      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/ajj35CJ4S5E' />
-    </div>
-    <div className="player-wrapper">
-      <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/rTsa8v35PA8' />
-    </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/BYnmDYNd17E' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/kIJ0KtItvyE' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/9F0Ysf6Wh10' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/UoNQGHMAbFk' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/FRVhk3nJfo0' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/a5ZXV5vXU0w' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/9Tx631VnH40' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/ajj35CJ4S5E' />
+      </div>
+      <div className="player-wrapper">
+        <ReactPlayer className='react-player' controls width='100%' height='100%' url='https://youtu.be/rTsa8v35PA8' />
+      </div>
     </div>
   );
 }
